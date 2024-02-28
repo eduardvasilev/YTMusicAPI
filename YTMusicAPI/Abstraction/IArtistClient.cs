85bee4589c69d5e2f87231f93afcb5f17314d01c
Eduard Vasilev <daetozheed@gmail.com>
2024-02-29 00:33:11 +0700

    Added annotation

--- YTMusicAPI/Abstraction/IArtistClient.cs
+++ YTMusicAPI/Abstraction/IArtistClient.cs
@@ -2,5 +2,11 @@
 
 public interface IArtistClient
 {
+    /// <summary>
+    /// Returns main picture of an artist 
+    /// </summary>
+    /// <param name="artistUrl">Url of an artist</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     Task<string> GetArtistImageAsync(string artistUrl, CancellationToken cancellationToken);
 }
